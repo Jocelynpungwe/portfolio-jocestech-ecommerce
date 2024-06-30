a54f055b714a53d27d52a3a71e0217f032ff7851
Jocelyn Nemba Pungwe <jocelynpungwe1997@gmail.com>
2024-06-29 20:11:42 -0400

    fix auth controller

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -7,8 +7,8 @@ const authenticateUser = async (req, res, next) => {
   }
 
   try {
-    const { name, userId, role } = verifyJWT(token)
-    req.user = { name, userId, role }
+    const { name, userId, role, email } = verifyJWT(token)
+    req.user = { name, email, userId, role }
     next()
   } catch (error) {
     throw new UnauthorizedError('Authentication Invalid')
